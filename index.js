e33b65a9103ff458364f8c36200f0edfb5736ba9
James Kao <james@thekao.com>
2014-06-17 10:30:50 -0700

    first commit - sticky footer navbar example

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+app.set('port', (process.env.PORT || 8080));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync('index.html','utf8'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
